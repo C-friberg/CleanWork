5df7f07079e7d8fb9bc3fb5cffb4285b1b6958a3
C-Friberg <you@example.com>
2026-05-25 09:06:13 +0200

    target input before implementing chrome storage

--- apps/extension/popup.js
+++ apps/extension/popup.js
@@ -1,2 +1,8 @@
 const container = document.getElementById("popup-container"); 
 
+const hideShortsHome = document.getElementById("hide-shorts-homepage"); 
+const hideShortsSearch = document.getElementById("hide-shorts-search");
+const hideRecomendations = document.getElementById("hide-recomendations");
+
+// Klick på checkbox ska spara i chrome.storage! 
+
